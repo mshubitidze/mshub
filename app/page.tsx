756aaa66b28b08f559b0633d8cf24d346fc91208
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2023-12-26 20:24:52 +0400

    dvh

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const [gh, setGh] = useState("https://github.com/mshubitidze");
   const [x, setX] = useState("https://x.com/_mshub");
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-2 text-lg">
+    <main className="flex min-h-[100dvh] flex-col items-center justify-center gap-2 text-lg">
       <div className="h-10">
         {x.split("").map((l, idx) => (
           <button
