5c35a5b34646e635325db1c0a58148373ab86799
Misho Shubitidze <misho@mshub.dev>
2024-06-07 01:58:38 +0400

    fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 export default function sitemap() {
-  return {
-    url: `https://mshub.dev`,
-    lastModified: new Date().toISOString().split('T')[0],
-  }
+  return [
+    {
+      url: `https://mshub.dev`,
+      lastModified: new Date().toISOString().split('T')[0],
+    },
+  ]
 }
