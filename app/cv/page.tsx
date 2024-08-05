a94890c96ec99dc9752f84dcc8f991b925b8502e
Misho Shubitidze <misho@mshub.dev>
2024-08-06 00:32:44 +0400

    add white cv

---
+++ app/cv/page.tsx
@@ -0,0 +1,6 @@
+import { redirect } from 'next/navigation'
+import { cv } from '../lib/constants'
+
+export default function Page() {
+  return redirect(cv)
+}
---
+++ app/lib/constants.ts
@@ -0,0 +1,2 @@
+export const cv =
+  'https://utfs.io/f/d138f42c-63b4-490a-8460-1bf6b6a64383-onq78x.pdf'
